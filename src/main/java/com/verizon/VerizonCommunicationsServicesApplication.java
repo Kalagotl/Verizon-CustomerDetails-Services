71f990868664c1b829d9f659aa5e80702ea39904
Dev1 <shekhardpd@gmail.com>
2024-06-22 12:23:43 +0530

    Updated code Pushed

--- src/main/java/com/verizon/VerizonCommunicationsServicesApplication.java
+++ src/main/java/com/verizon/VerizonCommunicationsServicesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication // Annotation to indicate this class as the main application class and enable Spring Boot auto-configuration
 public class VerizonCommunicationsServicesApplication {
-
+//Hello Complete code Commited nad Pushed 
 	public static void main(String[] args) {
 		
 		SpringApplication.run(VerizonCommunicationsServicesApplication.class, args);
